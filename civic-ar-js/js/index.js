8f83dfb6aae06376e6f332331085532aa00ef279
dlaumer <daniel.laumer@gmail.com>
2021-05-16 14:30:16 +0200

    Fixed compass problem with Android

--- civic-ar-js/js/index.js
+++ civic-ar-js/js/index.js
@@ -401,7 +401,7 @@ function deviceOrientationHandler(event) {
 
       //alert("Your device is reporting relative alpha values, so this compass won't point north :(");
      // var heading = 360 - alpha; //heading [0, 360)
-      heading = 360 - alpha;
+      heading = compassHeading(alpha, beta, gamma);
 
   }
 
@@ -440,6 +440,44 @@ function deviceOrientationHandler(event) {
 
 
 }
+
+function compassHeading(alpha, beta, gamma) {
+
+  // Convert degrees to radians
+  var alphaRad = alpha * (Math.PI / 180);
+  var betaRad = beta * (Math.PI / 180);
+  var gammaRad = gamma * (Math.PI / 180);
+
+  // Calculate equation components
+  var cA = Math.cos(alphaRad);
+  var sA = Math.sin(alphaRad);
+  var cB = Math.cos(betaRad);
+  var sB = Math.sin(betaRad);
+  var cG = Math.cos(gammaRad);
+  var sG = Math.sin(gammaRad);
+
+  // Calculate A, B, C rotation components
+  var rA = - cA * sG - sA * sB * cG;
+  var rB = - sA * sG + cA * sB * cG;
+  var rC = - cB * cG;
+
+  // Calculate compass heading
+  var compassHeading = Math.atan(rA / rB);
+
+  // Convert from half unit circle to whole unit circle
+  if(rB < 0) {
+    compassHeading += Math.PI;
+  }else if(rA < 0) {
+    compassHeading += 2 * Math.PI;
+  }
+
+  // Convert radians to degrees
+  compassHeading *= 180 / Math.PI;
+
+  return compassHeading;
+
+  }
+
 
 
 function distance(lat1, lon1, lat2, lon2, unit) {
